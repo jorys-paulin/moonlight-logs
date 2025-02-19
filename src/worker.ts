ae518814f79ce107dc363dd16ecd984c076e88f9
Jorys Paulin <jorys@jorys-paulin.fr>
2025-02-19 13:09:22 +0100

    feat: add initial cors support

--- src/worker.ts
+++ src/worker.ts
@@ -30,6 +30,20 @@ export default {
 		const url = new URL(request.url);
 
 		if (url.pathname === '/') {
+			// Options and CORS route
+			if (request.method === 'OPTIONS') {
+				return new Response(null, {
+					status: 204,
+					headers: {
+						Allow: 'GET, POST, DELETE, OPTIONS',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Methods': 'GET, POST, DELETE',
+						'Access-Control-Allow-Headers': 'Authorization',
+						'Access-Control-Max-Age': '86400',
+					},
+				});
+			}
+
 			// Homepage route
 			if (request.method === 'GET' && !url.searchParams.has('id')) {
 				return new Response(indexPage, { headers: { 'Content-Type': 'text/html;charset=UTF-8' } });
@@ -40,7 +54,7 @@ export default {
 				const contentType = request.headers.get('Content-Type');
 
 				if (!contentType) {
-					return new Response('Missing Content-Type', { status: 400 });
+					return new Response('Missing Content-Type', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				const uuid = crypto.randomUUID();
@@ -74,21 +88,21 @@ export default {
 						name = file.name;
 					}
 				} else {
-					return new Response('Unsupported Media Type', { status: 415 });
+					return new Response('Unsupported Media Type', { status: 415, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				if (!content) {
-					return new Response('Invalid Contents', { status: 400 });
+					return new Response('Invalid Contents', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// File exeeds size limit
 				if (content.byteLength > env.MOONLIGHT_MAX_FILE_SIZE) {
-					return new Response('Payload Too Large', { status: 413 });
+					return new Response('Payload Too Large', { status: 413, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Reject invalid file names
 				if (!name || !validateFilename(name)) {
-					return new Response('Invalid File Name', { status: 400 });
+					return new Response('Invalid File Name', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Upload file to KV
@@ -99,7 +113,7 @@ export default {
 					});
 				} catch (error) {
 					console.error(error);
-					return new Response('Internal Server Error', { status: 500 });
+					return new Response('Internal Server Error', { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Generate uploaded file URL
@@ -117,7 +131,11 @@ export default {
 					// Return a 201 Created response with URL in header and body
 					return new Response(redirectURL.toString(), {
 						status: 201,
-						headers: { Location: redirectURL.toString() },
+						headers: {
+							Location: redirectURL.toString(),
+							'Access-Control-Allow-Origin': '*',
+							'Access-Control-Expose-Headers': 'Location',
+						},
 					});
 				}
 			}
@@ -128,12 +146,12 @@ export default {
 
 				// Check for UUID
 				if (!uuid) {
-					return new Response('Missing UUID', { status: 400 });
+					return new Response('Missing UUID', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Check for valid UUID
 				if (!uuid.match(UUID_REGEX)) {
-					return new Response('Invalid UUID', { status: 400 });
+					return new Response('Invalid UUID', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Try to fetch the file from KV
@@ -144,13 +162,14 @@ export default {
 
 				// File doesn't exist
 				if (value === null) {
-					return new Response('File not found', { status: 404 });
+					return new Response('File not found', { status: 404, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Response headers
 				const headers = new Headers();
 				headers.set('Content-Type', 'text/plain; charset=UTF-8');
 				headers.set('Content-Disposition', `attachment; filename="${uuid}.txt"`);
+				headers.set('Access-Control-Allow-Origin', '*');
 
 				// File metadata
 				if (metadata) {
@@ -180,27 +199,27 @@ export default {
 
 				// Check for UUID
 				if (!uuid) {
-					return new Response('Missing UUID', { status: 400 });
+					return new Response('Missing UUID', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Check for valid UUID
 				if (!uuid.match(UUID_REGEX)) {
-					return new Response('Invalid UUID', { status: 400 });
+					return new Response('Invalid UUID', { status: 400, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				// Only allow token bearers to use this route
 				if (request.headers.get('Authorization') !== 'Bearer ' + env.MOONLIGHT_ADMIN_TOKEN) {
-					return new Response('Forbidden', { status: 403 });
+					return new Response('Forbidden', { status: 403, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
 				try {
 					await env.MOONLIGHT_LOGS.delete(uuid);
 				} catch (error) {
 					console.error(error);
-					return new Response('Internal Server Error', { status: 500 });
+					return new Response('Internal Server Error', { status: 500, headers: { 'Access-Control-Allow-Origin': '*' } });
 				}
 
-				return new Response(null, { status: 204 });
+				return new Response(null, { status: 204, headers: { 'Access-Control-Allow-Origin': '*' } });
 			}
 		}
 
